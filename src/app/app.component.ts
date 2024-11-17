e38eef29cbc5db1ae92e76b0b5e1034d2d99b850
Luiz Leroy <leroy291001@gmail.com>
2024-11-17 00:47:06 -0300

    feat: add action component to task card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,24 +5,56 @@ import { TaskService } from './services/task.service';
 import { Task } from './models/task.model';
 import { lastValueFrom } from 'rxjs';
 import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
+import { MenuItem, MessageService } from 'primeng/api';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,CardModule],
+  imports: [ HeaderComponent,CardModule,DividerModule,SplitButtonModule, ToastModule],
   templateUrl: './app.component.html',
   template:`<router-outlet />`,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [MessageService]
 })
 export class AppComponent implements OnInit {
-  tasks :Task[] = [];
- constructor(private taskService:TaskService){}
+tasks :Task[] = [];
+items: MenuItem[];
+ constructor(private taskService:TaskService){
+  this.items = [
+    {
+        label: 'Update',
+        command: () => {
+            this.update();
+        }
+    },
+    {
+        label: 'Delete',
+        command: () => {
+            this.delete();
+        }
+    },
+  ];
+ }
 
  ngOnInit(): void {
      this.getTasks();
  }
   async getTasks(){
     this.tasks = await lastValueFrom(this.taskService.getTasks());
-    console.log(this.tasks);
   }
+
+  save(action:string) : void {
+    console.log(action);
+  }
+
+  update() {
+    console.log('update');
+  }
+
+delete() {
+    console.log('delete');
+}
 }
